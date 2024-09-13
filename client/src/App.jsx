2be41c4b236ee5b5786f9b9d92e37057e3f80073
TOM SABU <tomsabu0000@gmail.com>
2024-09-14 02:46:33 +0530

    Add react-router-dom dependency and update App.jsx to include routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Home from "./components/Home";
+import About from "./components/About";
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;

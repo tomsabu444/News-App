fe1cd63e8f4968d2c89c99099235b43cf14669dc
TOM SABU <tomsabu0000@gmail.com>
2024-09-14 02:46:37 +0530

    Refactor component imports in App.jsx and add Layout component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from "./components/Home";
-import About from "./components/About";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
     <>

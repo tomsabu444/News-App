671d1f39fe0b67b02facb3bbb7d7ffc78eb895f7
TOM SABU <tomsabu0000@gmail.com>
2024-09-14 14:36:24 +0530

    Refactor project configuration files and update backend logic
    This commit refactors the project configuration files and updates the backend logic. It also adds a new route '/api/test' that returns a JSON response with the message 'Backend is working!'. The changes include importing necessary modules, setting up middleware for CORS and JSON parsing, and exporting the Firebase Function.

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,16 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// Middleware
+app.use(cors({ origin: true }));
+app.use(express.json()); // For parsing JSON
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Example route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Backend is working!' });
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Firebase Function Export
+exports.app = functions.https.onRequest(app);

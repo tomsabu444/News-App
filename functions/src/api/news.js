0099d2b30c2988215fe3b25736edb0f0672564fe
TOM SABU <tomsabu0000@gmail.com>
2024-09-14 15:24:14 +0530

    Refactor API endpoint URL in NewsFeed component ``

---
+++ functions/src/api/news.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+router.get("/news", async (req, res) => {
+  const apiKey = "2b153f670f3e1e16c1c9e014a32b679f"
+  const query = req.query.q || "example";
+  const language = req.query.lang || "en";
+  const country = req.query.country || "us";
+  const max = req.query.max || 10;
+  const page = req.query.page || 1;
+
+  const url = `https://gnews.io/api/v4/search?q=${query}&lang=${language}&country=${country}&max=${max}&page=${page}&apikey=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    res.status(500).json({ message: "Error fetching news data" });
+  }
+});
+
+module.exports = router;

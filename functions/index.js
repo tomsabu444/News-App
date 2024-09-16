70cee553ee25552a2e775b1401e37ea0a08605c4
TOM SABU <tomsabu0000@gmail.com>
2024-09-16 12:15:19 +0530

    Refactor CORS configuration in functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,33 @@ const app = express();
 
 
 // Middleware
-const corsOptions = {
-  origin: 'https://news-app-a591e.web.app'
-};
+const allowedDomain = 'https://news-app-a591e.web.app';
 
+// CORS middleware: Allow requests only from the allowed domain
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || origin === allowedDomain) {
+      callback(null, true); // Allow if no origin or the origin is allowedDomain
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block other origins
+    }
+  }
+};
 app.use(cors(corsOptions));
-app.use(express.json()); // For parsing JSON
+
+app.use((req, res, next) => {
+  const origin = req.get('Origin') || req.get('Referer');
+
+  // If origin or referer is not allowed, block the request
+  if (origin && origin.includes(allowedDomain)) {
+    next(); 
+  } else {
+    return res.status(403).json({ message: 'Forbidden: Access is denied.' });
+  }
+});
+
+
+app.use(express.json()); 
 
 // Routes
 const newsRoute = require("./src/api/news");
